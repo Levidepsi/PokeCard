11622f78515b85dcb41cc0d4bd054568f7c80c55
LeviDepsi <levidepsi7@gmail.com>
2021-09-20 10:46:47 +0800

    ReactCourse

---
+++ src/Pokedex.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import PokemonCard from './PokemonCard';
+import './Pokedex.css';
+
+class Pokedex extends Component {
+	render() {
+		let title;
+		if (this.props.isWinner) {
+			title = <h1 className="Pokedex-winner">Winning Hand</h1>;
+		} else {
+			title = <h1 className="Pokedex-loser">Losing Hand</h1>;
+		}
+		return (
+			<div className="Pokedex">
+				{title}
+				<h4>Total Experience: {this.props.exp}</h4>
+				<div className="Pokedex-cards">
+					{this.props.pokemon.map((p) => (
+						<PokemonCard id={p.id} name={p.name} type={p.type} exp={p.base_experience} />
+					))}
+				</div>
+			</div>
+		);
+	}
+}
+
+export default Pokedex;
---
+++ src/PokemonCard.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react'
+import './Pokecard.css'
+
+const POKE_API = `https://assets.pokemon.com/assets/cms2/img/pokedex/detail/`
+
+const padToThree = (number) => (number <= 999 ? `00${number}`.slice(-3) : number)
+
+class PokemonCard extends Component {
+    render() {
+        const imgSrc = `${POKE_API}${padToThree(this.props.id)}.png`
+
+        return(
+            <div className='PokemonCard' >
+                <h1 className='Pokecard-title'>{this.props.name}</h1>
+                <div className='Pokecard-img' >
+                <img src={imgSrc} alt='' />
+                </div>
+              
+                <div className='Pokecard-data' >Type: {this.props.type}</div>
+                <div className='Pokecard-data' >EXP: {this.props.exp}</div>
+            </div>
+        )
+    }
+}
+
+export default PokemonCard
